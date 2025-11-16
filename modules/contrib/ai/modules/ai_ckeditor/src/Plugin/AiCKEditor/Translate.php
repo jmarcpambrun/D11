031994461ab489d29eb709c9db6d0fac38900efc
jmarcpambrun <jmarc.pambrun@free.fr>
2025-11-16 11:45:23 +0100

    To reactivare lang in aickeditor module : modules/contrib/ai/modules/ai_ckeditor/src/Plugin/AiCKEditor/Translate.php

--- modules/contrib/ai/modules/ai_ckeditor/src/Plugin/AiCKEditor/Translate.php
+++ modules/contrib/ai/modules/ai_ckeditor/src/Plugin/AiCKEditor/Translate.php
@@ -153,16 +153,19 @@ final class Translate extends AiCKEditorPluginBase {
   public function buildCkEditorModalForm(array $form, FormStateInterface $form_state, array $settings = []) {
     $form = parent::buildCkEditorModalForm($form, $form_state);
 
+    $autocreate = $this->configuration['autocreate'] && $this->configuration['language_source'] == 'tax';
+
     $form['language'] = [
-      '#type' => $this->configuration['autocreate'] ? 'entity_autocomplete' : 'select',
+      #'#type' => $this->configuration['autocreate'] ? 'entity_autocomplete' : 'select',
+	  '#type' => $autocreate ? 'entity_autocomplete' : 'select',
       '#title' => $this->t('Choose language'),
       '#tags' => FALSE,
       '#required' => TRUE,
       '#weight' => 3,
       '#description' => $this->t('Selecting one of the options will translate the selected text.'),
     ];
-
-    if ($this->configuration['autocreate']) {
+	if ($autocreate) {
+    #if ($this->configuration['autocreate']) {
       $form['language']['#target_type'] = 'taxonomy_term';
       $form['language']['#selection_settings'] = [
         'target_bundles' => [$this->configuration['translate_vocabulary']],
@@ -175,9 +178,22 @@ final class Translate extends AiCKEditorPluginBase {
       }
     }
     else {
-      $form['language']['#options'] = $this->getTermOptions($this->configuration['translate_vocabulary']);
-    }
-
+      #$form['language']['#options'] = $this->getTermOptions($this->configuration['translate_vocabulary']);
+      if ($this->configuration['language_source'] == 'tax') {
+        $form['language']['#options'] = $this->getTermOptions($this->configuration['translate_vocabulary']);
+      }
+      else {
+        $site_languages = $this->languageManager->getLanguages();
+        $form['language']['#options'] = [];
+        foreach ($site_languages as $langcode => $language) {
+          $form['language']['#options'][$langcode] = $language->getName();
+        }
+        // Set default value if only one language is available.
+        if (count($form['language']['#options']) === 1) {
+          $form['language']['#default_value'] = key($form['language']['#options']);
+        }
+      }
+    }	
     return $form;
   }
 
