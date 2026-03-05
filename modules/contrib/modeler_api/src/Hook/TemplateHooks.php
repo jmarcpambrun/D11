7365c9dda0e217da451c5d8e4afd51d57eecb9f3
jmarcpambrun <jmarc.pambrun@free.fr>
2026-03-05 22:53:40 +0100

    modif de template hook pour pouvoir avoir accès à ECA configuration

--- modules/contrib/modeler_api/src/Hook/TemplateHooks.php
+++ modules/contrib/modeler_api/src/Hook/TemplateHooks.php
@@ -22,13 +22,23 @@ class TemplateHooks {
   /**
    * Implements hook_library_info_alter().
    */
-  #[Hook('library_info_alter')]
+#[Hook('library_info_alter')]
   public function libraryInfoAlter(array &$libraries, string $extension): void {
     if ($extension === 'modeler_api' && isset($libraries['template_token_selector'])) {
+      // URL du token CSRF (existant dans Drupal)
       $libraries['template_token_selector']['drupalSettings']['modeler_api']['token_url'] = Url::fromRoute('system.csrftoken')->toString();
-      $libraries['template_token_selector']['drupalSettings']['modeler_api']['template_apply_url'] = Url::fromRoute('modeler_api.apply_template')->toString();
+
+      // URL pour appliquer le template (sécurisée)
+      try {
+         $libraries['template_token_selector']['drupalSettings']['modeler_api']['template_apply_url'] = Url::fromRoute('modeler_api.apply_template')->toString();
+      }
+      catch (\Symfony\Component\Routing\Exception\RouteNotFoundException $e) {
+        // Route inexistante : on logue et on met un fallback vide.
+        \Drupal::logger('modeler_api')->warning('La route "modeler_api.apply_template" n’existe pas. template_apply_url laissé vide.');
+        $libraries['template_token_selector']['drupalSettings']['modeler_api']['template_apply_url'] = '';
+      }
     }
-  }
+ } 
 
   /**
    * Implements hook_page_attachments().
