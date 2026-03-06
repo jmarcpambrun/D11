1019addf19bde1213adffe260d4fcfaa26ea7ce8
jmarcpambrun <jmarc.pambrun@free.fr>
2026-03-06 10:00:17 +0100

    Issue #3532309: Fix Deprecation notice from DeleteAction class causes errors in ECA module

--- modules/contrib/entity/src/Plugin/Action/DeleteAction.php
+++ modules/contrib/entity/src/Plugin/Action/DeleteAction.php
@@ -4,7 +4,9 @@ namespace Drupal\entity\Plugin\Action;
 
 use Drupal\Core\Action\Plugin\Action\DeleteAction as CoreDeleteAction;
 
-@trigger_error('\Drupal\entity\Plugin\Action\DeleteAction has been deprecated in favor of \Drupal\Core\Action\Plugin\Action\DeleteAction. Use that instead.');
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\TempStore\PrivateTempStoreFactory;
 
 /**
  * Redirects to an entity deletion form.
@@ -17,4 +19,14 @@ use Drupal\Core\Action\Plugin\Action\DeleteAction as CoreDeleteAction;
  *   deriver = "Drupal\entity\Plugin\Action\Derivative\DeleteActionDeriver",
  * )
  */
-class DeleteAction extends CoreDeleteAction {}
+class DeleteAction extends CoreDeleteAction {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, PrivateTempStoreFactory $temp_store_factory, AccountInterface $current_user) {
+    @trigger_error('\Drupal\entity\Plugin\Action\DeleteAction has been deprecated in favor of \Drupal\Core\Action\Plugin\Action\DeleteAction. Use that instead.', E_USER_DEPRECATED);
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $entity_type_manager, $temp_store_factory, $current_user);
+  }
+}
+
