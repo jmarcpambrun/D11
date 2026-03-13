a5d94562130337679b8d77bb27d9cd56387e27e2
jmarcpambrun <jmarc.pambrun@free.fr>
2026-03-13 16:53:45 +0100

    On remet sites

---
+++ sites/default/files/php/twig/69b42d6f3c82f_status-report-grouped.htm_Oe12NZYcn_7lHUPABtUp_JnkH/GSirE33wfn7RNeR-JjTalSf5X4vyy4qL59JFJB-GYSg.php
@@ -0,0 +1,191 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\CoreExtension;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+use Twig\TemplateWrapper;
+
+/* themes/contrib/seven/templates/status-report-grouped.html.twig */
+class __TwigTemplate_f30bc0469a5f9809493ba767a587f0ef extends Template
+{
+    private Source $source;
+    /**
+     * @var array<string, Template>
+     */
+    private array $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->extensions[SandboxExtension::class];
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = []): iterable
+    {
+        $macros = $this->macros;
+        // line 19
+        yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->extensions['Drupal\Core\Template\TwigExtension']->attachLibrary("core/drupal.collapse"), "html", null, true);
+        yield "
+";
+        // line 20
+        yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->extensions['Drupal\Core\Template\TwigExtension']->attachLibrary("seven/drupal.responsive-detail"), "html", null, true);
+        yield "
+
+<div class=\"system-status-report\">
+  ";
+        // line 23
+        $context['_parent'] = $context;
+        $context['_seq'] = CoreExtension::ensureTraversable(($context["grouped_requirements"] ?? null));
+        foreach ($context['_seq'] as $context["_key"] => $context["group"]) {
+            // line 24
+            yield "    <div class=\"system-status-report__requirements-group\">
+      <h3 id=\"";
+            // line 25
+            yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, CoreExtension::getAttribute($this->env, $this->source, $context["group"], "type", [], "any", false, false, true, 25), "html", null, true);
+            yield "\">";
+            yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, CoreExtension::getAttribute($this->env, $this->source, $context["group"], "title", [], "any", false, false, true, 25), "html", null, true);
+            yield "</h3>
+      ";
+            // line 26
+            $context['_parent'] = $context;
+            $context['_seq'] = CoreExtension::ensureTraversable(CoreExtension::getAttribute($this->env, $this->source, $context["group"], "items", [], "any", false, false, true, 26));
+            foreach ($context['_seq'] as $context["_key"] => $context["requirement"]) {
+                // line 27
+                yield "        <details class=\"system-status-report__entry system-status-report__entry--";
+                yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, CoreExtension::getAttribute($this->env, $this->source, $context["group"], "type", [], "any", false, false, true, 27), "html", null, true);
+                yield " color-";
+                yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, CoreExtension::getAttribute($this->env, $this->source, $context["group"], "type", [], "any", false, false, true, 27), "html", null, true);
+                yield "\" open>
+          ";
+                // line 29
+                $context["summary_classes"] = ["system-status-report__status-title", ((CoreExtension::inFilter(CoreExtension::getAttribute($this->env, $this->source,                 // line 31
+$context["group"], "type", [], "any", false, false, true, 31), ["warning", "error"])) ? (("system-status-report__status-icon system-status-report__status-icon--" . CoreExtension::getAttribute($this->env, $this->source, $context["group"], "type", [], "any", false, false, true, 31))) : (""))];
+                // line 34
+                yield "          <summary";
+                yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->extensions['Drupal\Core\Template\TwigExtension']->createAttribute(["class" => ($context["summary_classes"] ?? null)]), "html", null, true);
+                yield " role=\"button\">
+            ";
+                // line 35
+                if ((($tmp = CoreExtension::getAttribute($this->env, $this->source, $context["requirement"], "severity_title", [], "any", false, false, true, 35)) && $tmp instanceof Markup ? (string) $tmp : $tmp)) {
+                    // line 36
+                    yield "              <span class=\"visually-hidden\">";
+                    yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, CoreExtension::getAttribute($this->env, $this->source, $context["requirement"], "severity_title", [], "any", false, false, true, 36), "html", null, true);
+                    yield "</span>
+            ";
+                }
+                // line 38
+                yield "            ";
+                yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, CoreExtension::getAttribute($this->env, $this->source, $context["requirement"], "title", [], "any", false, false, true, 38), "html", null, true);
+                yield "
+          </summary>
+          <div class=\"system-status-report__entry__value\">
+            ";
+                // line 41
+                yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, CoreExtension::getAttribute($this->env, $this->source, $context["requirement"], "value", [], "any", false, false, true, 41), "html", null, true);
+                yield "
+            ";
+                // line 42
+                if ((($tmp = CoreExtension::getAttribute($this->env, $this->source, $context["requirement"], "description", [], "any", false, false, true, 42)) && $tmp instanceof Markup ? (string) $tmp : $tmp)) {
+                    // line 43
+                    yield "              <div class=\"description\">";
+                    yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, CoreExtension::getAttribute($this->env, $this->source, $context["requirement"], "description", [], "any", false, false, true, 43), "html", null, true);
+                    yield "</div>
+            ";
+                }
+                // line 45
+                yield "          </div>
+        </details>
+      ";
+            }
+            $_parent = $context['_parent'];
+            unset($context['_seq'], $context['_key'], $context['requirement'], $context['_parent']);
+            $context = array_intersect_key($context, $_parent) + $_parent;
+            // line 48
+            yield "    </div>
+  ";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_key'], $context['group'], $context['_parent']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 50
+        yield "</div>
+";
+        $this->env->getExtension('\Drupal\Core\Template\TwigExtension')
+            ->checkDeprecations($context, ["grouped_requirements"]);        yield from [];
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getTemplateName(): string
+    {
+        return "themes/contrib/seven/templates/status-report-grouped.html.twig";
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function isTraitable(): bool
+    {
+        return false;
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getDebugInfo(): array
+    {
+        return array (  128 => 50,  121 => 48,  113 => 45,  107 => 43,  105 => 42,  101 => 41,  94 => 38,  88 => 36,  86 => 35,  81 => 34,  79 => 31,  78 => 29,  71 => 27,  67 => 26,  61 => 25,  58 => 24,  54 => 23,  48 => 20,  44 => 19,);
+    }
+
+    public function getSourceContext(): Source
+    {
+        return new Source("", "themes/contrib/seven/templates/status-report-grouped.html.twig", "/var/www/D11/themes/contrib/seven/templates/status-report-grouped.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = ["for" => 23, "set" => 29, "if" => 35];
+        static $filters = ["escape" => 19];
+        static $functions = ["attach_library" => 19, "create_attribute" => 34];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['for', 'set', 'if'],
+                ['escape'],
+                ['attach_library', 'create_attribute'],
+                $this->source
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
---
+++ sites/default/files/php/twig/69b42d6f3c82f_update-version.html.twig_IwPflht1yMecCmr17fNyafpGW/3oloxVH47Um1gTQMiKJ69044TF9asJfXmq28qvz8g1I.php
@@ -0,0 +1,155 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\CoreExtension;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+use Twig\TemplateWrapper;
+
+/* @update/update-version.html.twig */
+class __TwigTemplate_147b739ea2d5f905154ee3a1f131f4b1 extends Template
+{
+    private Source $source;
+    /**
+     * @var array<string, Template>
+     */
+    private array $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->extensions[SandboxExtension::class];
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = []): iterable
+    {
+        $macros = $this->macros;
+        // line 28
+        yield "<div class=\"";
+        yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, CoreExtension::getAttribute($this->env, $this->source, ($context["attributes"] ?? null), "class", [], "any", false, false, true, 28), "html", null, true);
+        yield " project-update__version\"";
+        yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->extensions['Drupal\Core\Template\TwigExtension']->withoutFilter(($context["attributes"] ?? null), "class"), "html", null, true);
+        yield ">
+  <div class=\"clearfix\">
+    <div class=\"project-update__version-title layout-column layout-column--quarter\">";
+        // line 30
+        yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, ($context["title"] ?? null), "html", null, true);
+        yield "</div>
+    <div class=\"project-update__version-details layout-column layout-column--quarter\">
+      <a href=\"";
+        // line 32
+        yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, CoreExtension::getAttribute($this->env, $this->source, ($context["version"] ?? null), "release_link", [], "any", false, false, true, 32), "html", null, true);
+        yield "\">";
+        yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, CoreExtension::getAttribute($this->env, $this->source, ($context["version"] ?? null), "version", [], "any", false, false, true, 32), "html", null, true);
+        yield "</a>
+      <span class=\"project-update__version-date\">(";
+        // line 33
+        yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->extensions['Twig\Extension\CoreExtension']->formatDate(CoreExtension::getAttribute($this->env, $this->source, ($context["version"] ?? null), "date", [], "any", false, false, true, 33), "Y-M-d"), "html", null, true);
+        yield ")</span>
+    </div>
+    <div class=\"layout-column layout-column--half\">
+      <ul class=\"project-update__version-links\">
+        <li class=\"project-update__release-notes-link\">
+          <a href=\"";
+        // line 38
+        yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, CoreExtension::getAttribute($this->env, $this->source, ($context["version"] ?? null), "release_link", [], "any", false, false, true, 38), "html", null, true);
+        yield "\">";
+        yield $this->extensions['Drupal\Core\Template\TwigExtension']->renderVar(t("Release notes"));
+        yield "</a>
+        </li>
+        ";
+        // line 40
+        if ((($tmp = ($context["core_compatibility_details"] ?? null)) && $tmp instanceof Markup ? (string) $tmp : $tmp)) {
+            // line 41
+            yield "          <li class=\"project-update__compatibility-details\">
+            ";
+            // line 42
+            yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, ($context["core_compatibility_details"] ?? null), "html", null, true);
+            yield "
+          </li>
+        ";
+        }
+        // line 45
+        yield "      </ul>
+    </div>
+  </div>
+</div>
+";
+        $this->env->getExtension('\Drupal\Core\Template\TwigExtension')
+            ->checkDeprecations($context, ["attributes", "title", "version", "core_compatibility_details"]);        yield from [];
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getTemplateName(): string
+    {
+        return "@update/update-version.html.twig";
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function isTraitable(): bool
+    {
+        return false;
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getDebugInfo(): array
+    {
+        return array (  89 => 45,  83 => 42,  80 => 41,  78 => 40,  71 => 38,  63 => 33,  57 => 32,  52 => 30,  44 => 28,);
+    }
+
+    public function getSourceContext(): Source
+    {
+        return new Source("", "@update/update-version.html.twig", "/var/www/D11/core/modules/update/templates/update-version.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = ["if" => 40];
+        static $filters = ["escape" => 28, "without" => 28, "date" => 33, "t" => 38];
+        static $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['if'],
+                ['escape', 'without', 'date', 't'],
+                [],
+                $this->source
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
