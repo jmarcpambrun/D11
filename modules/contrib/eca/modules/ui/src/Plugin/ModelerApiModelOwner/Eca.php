571da0d0294d37c863ad7f602b511bb7eec25b4a
jmarcpambrun <jmarc.pambrun@free.fr>
2026-05-02 13:05:33 +0200

    Revert "Retour à la version d'origine avec  dans drush cr [error]  UnexpectedValueException: base:base:admin/config has no corresponding route. in Drupal\Core\Url->getRouteName() (line 574 of /var/www/D11/core/lib/Drupal/Core/Url.php). return 'route:entity.eca.collection'; changé en return 'admin/config/workflow/eca';"
    This reverts commit f82b5f07fcef59af6ff9a72523feabfc06112c5c.

--- modules/contrib/eca/modules/ui/src/Plugin/ModelerApiModelOwner/Eca.php
+++ modules/contrib/eca/modules/ui/src/Plugin/ModelerApiModelOwner/Eca.php
@@ -138,7 +138,7 @@ class Eca extends ModelOwnerBase {
    * {@inheritdoc}
    */
   public function configEntityBasePath(): string {
-    return 'base:admin/config/workflow';
+    return 'route:entity.eca.collection';
   }
 
   /**
